c8cd352273d7788f5971eb36a77f6f0c7fa489f4
Jean Viegas <jeanmelg@gmail.com>
2021-10-14 19:23:09 -0300

    exercicio JS e Logica ate o 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -0,0 +1,9 @@
+let n = 5;
+
+for(let i = 0;i < n; i+=1){
+  let line = ""
+  for(let j = 0; j < n; j+=1){
+    line += "*"
+  }
+  console.log(line)
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -0,0 +1,8 @@
+let n = 5;
+let line = ""
+for(let i = 0;i < n; i+=1){
+
+    line += "*"
+
+  console.log(line)
+}
