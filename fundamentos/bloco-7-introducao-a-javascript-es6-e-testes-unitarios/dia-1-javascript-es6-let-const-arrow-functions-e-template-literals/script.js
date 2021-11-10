9a08586ea57d7097daeab4fc7022b14ad1ffde5f
Jean Viegas <jeanmelg@gmail.com>
2021-11-10 18:19:22 -0300

    finaliza parte um

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,20 @@
+/* function testingScope(escopo) {
+  let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+  if (escopo === true) {
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true); */
+
+/* const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  // Seu código aqui.
+
+console.log(oddsAndEvens.sort((a, b) => a - b)); // será necessário alterar essa linha 😉 */
+
+
